e1e7cf3b560ef3156df0e080237c127b5cb20e9c
mmuralim <mmuralim2000@hotmail.com>
2021-02-26 16:42:40 +0530

    Update hungry.py
    as per your request added 'Y' as one of the option for hungry

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry=input("are you hungry?")
-if hungry=="yes" and hungry=="y":
+if hungry=="yes" and hungry=="y" and hungry=="Y":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
